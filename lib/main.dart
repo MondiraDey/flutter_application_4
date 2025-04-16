265e27974d2a8dca94f9dcb569a5681b39ce640a
MondiraDey <dey15-5735@diu.edu.bd>
2025-04-16 12:31:15 +0600

    18_mart

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,64 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyDrawerApp());
+  runApp(ProfileApp());
 }
 
-class MyDrawerApp extends StatelessWidget {
+class ProfileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DrawerScreen(),
+      home: ProfilePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class DrawerScreen extends StatefulWidget {
+class ProfilePage extends StatefulWidget {
   @override
-  _DrawerScreenState createState() => _DrawerScreenState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
-class _DrawerScreenState extends State<DrawerScreen> with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _slideAnimation;
-  bool _isDrawerOpen = false;
+class _ProfilePageState extends State<ProfilePage> {
+  File? _imageFile;
 
-  @override
-  void initState() {
-    super.initState();
-    // Animation controller for drawer
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 500),
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 85,
     );
 
-    _slideAnimation = Tween<Offset>(
-      begin: Offset(-1, 0), // Initially off-screen to the left
-      end: Offset(0, 0), // Fully visible
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-  }
-
-  void _toggleDrawer() {
-    setState(() {
-      if (_isDrawerOpen) {
-        _controller.reverse();
-      } else {
-        _controller.forward();
-      }
-      _isDrawerOpen = !_isDrawerOpen;
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Custom Drawer Animation'),
-      ),
+      appBar: AppBar(title: Text('My Profile')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: _toggleDrawer,
-          child: Text('Toggle Drawer'),
-        ),
-      ),
-      // Side drawer using SlideTransition animation
-      drawer: SlideTransition(
-        position: _slideAnimation,
-        child: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              ListTile(
-                title: Text('Item 1'),
-                onTap: () {
-                  // Close drawer after selection
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text('Item 2'),
-                onTap: () {
-                  // Close drawer after selection
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Circular Avatar
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: _imageFile != null
+                  ? FileImage(_imageFile!)
+                  : AssetImage('assets/default_avatar.png') as ImageProvider,
+              backgroundColor: Colors.grey[200],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: _pickImage,
+              icon: Icon(Icons.photo),
+              label: Text('Change Avatar'),
+            ),
+          ],
         ),
       ),
     );
