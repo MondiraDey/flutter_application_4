e75e99ce0a81905cfe0b0d46912ce2b4627bf1a0
MondiraDey <dey15-5735@diu.edu.bd>
2025-04-16 12:21:13 +0600

    mart_19

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,109 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MySettingsApp());
+  runApp(MyDrawerApp());
 }
 
-class MySettingsApp extends StatefulWidget {
-  @override
-  _MySettingsAppState createState() => _MySettingsAppState();
-}
-
-class _MySettingsAppState extends State<MySettingsApp> {
-  bool _isDarkMode = false;
-
+class MyDrawerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: SettingsScreen(
-        isDarkMode: _isDarkMode,
-        onToggleDarkMode: (bool value) {
-          setState(() {
-            _isDarkMode = value;
-          });
-        },
-      ),
+      home: DrawerScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class SettingsScreen extends StatelessWidget {
-  final bool isDarkMode;
-  final ValueChanged<bool> onToggleDarkMode;
+class DrawerScreen extends StatefulWidget {
+  @override
+  _DrawerScreenState createState() => _DrawerScreenState();
+}
 
-  const SettingsScreen({
-    required this.isDarkMode,
-    required this.onToggleDarkMode,
-  });
+class _DrawerScreenState extends State<DrawerScreen> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+  bool _isDrawerOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Animation controller for drawer
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(-1, 0), // Initially off-screen to the left
+      end: Offset(0, 0), // Fully visible
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  void _toggleDrawer() {
+    setState(() {
+      if (_isDrawerOpen) {
+        _controller.reverse();
+      } else {
+        _controller.forward();
+      }
+      _isDrawerOpen = !_isDrawerOpen;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
-      body: ListView(
-        children: [
-          SwitchListTile(
-            title: Text('Dark Mode'),
-            subtitle: Text('Enable or disable dark theme'),
-            value: isDarkMode,
-            onChanged: onToggleDarkMode,
-            secondary: Icon(Icons.dark_mode),
+      appBar: AppBar(
+        title: Text('Custom Drawer Animation'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _toggleDrawer,
+          child: Text('Toggle Drawer'),
+        ),
+      ),
+      // Side drawer using SlideTransition animation
+      drawer: SlideTransition(
+        position: _slideAnimation,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Close drawer after selection
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Close drawer after selection
+                  Navigator.pop(context);
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
